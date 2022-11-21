88adc0e945ab3fbac0c7f24a057ac112a17750ad
Bigginlasley <bigginlasley@gmail.com>
2022-11-21 01:06:19 -0700

    added stuff to check and make sure the body contains actual messages and containes update,delete,create

--- Assignment2/lambda_handler.py
+++ Assignment2/lambda_handler.py
@@ -4,10 +4,22 @@ import boto3
 def lambda_handler(event, context):
     client = boto3.client("sqs")
     msg = json.dumps(event['body'])
-    response = client.send_message(
-        QueueUrl = "https://sqs.us-east-1.amazonaws.com/685955345753/cs5260-requests",
-        MessageBody = msg)
-    return {
-        'statusCode': response["ResponseMetadata"]["HTTPStatusCode"],
-        'body': json.dumps(response["ResponseMetadata"])
-        }
+    if(msg):
+        if msg.find('create') != -1 or msg.find('delete') != -1 or msg.find('update') != -1:
+            response = client.send_message(
+                QueueUrl = "https://sqs.us-east-1.amazonaws.com/685955345753/cs5260-requests",
+                MessageBody = msg)
+            return {
+                'statusCode': response["ResponseMetadata"]["HTTPStatusCode"],
+                'body': json.dumps(response["ResponseMetadata"])
+                }
+        else:
+            return {
+                'statusCode' : 499,
+                'body' : "Failed to process: message body didn't contain proper type"
+                }
+    else:
+        return {
+            'statusCode' : 499,
+            'body' : "Failed to process: message body was empty"
+            }
